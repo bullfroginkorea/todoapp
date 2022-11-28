a47093dc04bf5f4727cb3d50a788cde8ce00783c
bullfroginkorea <lucky7909@naver.com>
2022-11-29 00:55:06 +0900

    8장 form으로 post , 정보 요청으로 감 ,body-parser,  .get()콜백

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-
+// body-parser 쓰려면~ ! body-parser은 요쳥 데이터 해석 도와줌 
+app.use(express.urlencoded({extended: true})) 
 app.listen(8080,function () {
     console.log('listening on 8080')
 });
@@ -13,4 +14,13 @@ app.get('/beauty',function(요청,응답) {
 })
 app.get('/',function (요청,응답) {
     응답.sendFile(__dirname+'/index.html')
+})
+app.get('/write',function (요청,응답) {
+    응답.sendFile(__dirname+'/write.html')
+})
+//7장 어떤사람이 /add 경로로 POST요청을 하면.. 
+// ??을 해주세요
+app.post('/add',function (요청,응답) {
+    응답.send('전송완료')
+    console.log(요청.body.title)
 })
