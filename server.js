40650852cd2645557471dec7203e793df872a10a
bullfroginkorea <lucky7909@naver.com>
2022-11-28 23:53:16 +0900

    6장

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/pet',function(요청,응답) {
     응답.send('펫쇼핑할 수 있는 사이트입니다.');
 })
 app.get('/beauty',function(요청,응답) {
-    응답.send('뷰티용품사세요.');
+    응답.send('뷰티용품사세요잉.');
 })
 app.get('/',function (요청,응답) {
     응답.sendFile(__dirname+'/index.html')
